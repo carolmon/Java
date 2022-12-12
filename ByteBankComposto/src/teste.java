310c8aaee9d660332b7325656ee15a888f68740e
carolmon <40300739+carolmon@users.noreply.github.com>
2022-12-12 17:30:59 -0300

    Continuation from Alura's Java couse Java OO

---
+++ ByteBankComposto/src/teste.java
@@ -0,0 +1,8 @@
+
+public class teste {
+	public static void main(String[] args) {
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.titular = new Cliente();
+		contaDaMarcela.titular.nome = "Marcela";
+	}
+}
